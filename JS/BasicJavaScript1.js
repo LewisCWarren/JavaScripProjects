957c3281886877ea993a2aa18fbc58fcd37abada
Lewis <lewis.c.warren@gmail.com>
2019-11-14 14:01:19 -0800

    Add Basic JavaScript assignment from page 58.

---
+++ JS/BasicJavaScript1.js
@@ -0,0 +1,14 @@
+var BestVillain = "Joker", NextBest = " Darth Vader", ThirdBest = " Magneto";
+
+var BestVillain = BestVillain.fontcolor("purple"); var NextBest = NextBest.fontcolor("Black"); var ThirdBest = ThirdBest.fontcolor("red");
+
+
+document.write(BestVillain + NextBest + ThirdBest);
+
+
+function MyFirstFunction () {
+var green = "this text is green";
+var result = green.fontcolor("green");
+document.getElementById(GreenText).innerHTML = result;
+
+}
