7ac262a12a072c221dad205830a84e63773aa77c
Lewis <lewis.c.warren@gmail.com>
2019-11-14 15:44:33 -0800

    Add JavaScript and HTML updates per assignment.

--- JS/BasicJavaScript1.js
+++ JS/BasicJavaScript1.js
@@ -1,14 +1,9 @@
-var BestVillain = "Joker", NextBest = " Darth Vader", ThirdBest = " Magneto";
 
-var BestVillain = BestVillain.fontcolor("purple"); var NextBest = NextBest.fontcolor("Black"); var ThirdBest = ThirdBest.fontcolor("red");
-
-
-document.write(BestVillain + NextBest + ThirdBest);
 
 
 function MyFirstFunction () {
 var green = "this text is green";
 var result = green.fontcolor("green");
-document.getElementById(GreenText).innerHTML = result;
+document.getElementById("GreenText").innerHTML = result;
 
 }
---
+++ JS/BasicJavaScript2.js
@@ -0,0 +1,9 @@
+
+
+
+function MyFunction() {
+    var sentence = "Hello,";
+        sentence += " World!";
+        document.getElementById("Concatenate").innerHTML = sentence;
+}
+
